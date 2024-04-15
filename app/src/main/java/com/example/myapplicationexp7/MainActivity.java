eb8002f00a383a755b15761f92087bceb60328ac
Anvita <kumaranvita30@gmail.com>
2024-04-16 00:18:12 +0530

    init

--- app/src/main/java/com/example/myapplicationexp7/MainActivity.java
+++ app/src/main/java/com/example/myapplicationexp7/MainActivity.java
@@ -3,121 +3,73 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
-    private TextView textViewResult;
-    private String input = "";
-    private double num1 = 0, num2 = 0;
-    private String operator = "";
+    EditText etFirstNumber, etSecondNumber;
+    TextView tvResult;
+    Button btnAdd, btnSubtract, btnMultiply, btnDivide, btnClear;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Initialize views
-        textViewResult = findViewById(R.id.textViewResult);
-        // Set click listeners for number buttons
-        setNumberButtonClickListeners();
-        // Set click listeners for operator buttons
-        setOperatorButtonClickListeners();
-        // Set click listener for equals button
-        setEqualsButtonClickListener();
-        // Set click listener for clear button
-        setClearButtonClickListener();
-    }
-    // Set click listeners for number buttons
-    private void setNumberButtonClickListeners() {
-        int[] numberButtonIds = {R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
-                R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9};
-        for (int buttonId : numberButtonIds) {
-            Button button = findViewById(buttonId);
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Button clickedButton = (Button) view;
-                    input += clickedButton.getText().toString();
-                    updateResult();
-                }
-            });
-        }
-    }
-    // Set click listeners for operator buttons
-    private void setOperatorButtonClickListeners() {
-        int[] operatorButtonIds = {R.id.buttonAdd, R.id.buttonSubtract, R.id.buttonMultiply, R.id.buttonDivide};
-        for (int buttonId : operatorButtonIds) {
-            Button button = findViewById(buttonId);
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Button clickedButton = (Button) view;
-                    operator = clickedButton.getText().toString();
-                    if (!input.isEmpty()) {
-                        num1 = Double.parseDouble(input);
-                        input = "";
-                    }
-                }
-            });
-        }
-    }
-    // Set click listener for equals button
-    private void setEqualsButtonClickListener() {
-        Button equalsButton = findViewById(R.id.buttonEquals);
-        equalsButton.setOnClickListener(new View.OnClickListener() {
+        etFirstNumber = findViewById(R.id.et_first_number);
+        etSecondNumber = findViewById(R.id.et_second_number);
+        tvResult = findViewById(R.id.tv_result);
+        btnAdd = findViewById(R.id.btn_add);
+        btnSubtract = findViewById(R.id.btn_subtract);
+        btnMultiply = findViewById(R.id.btn_multiply);
+        btnDivide = findViewById(R.id.btn_divide);
+        btnClear = findViewById(R.id.btn_clear);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                if (!input.isEmpty()) {
-                    num2 = Double.parseDouble(input);
-                    calculateResult();
-                    operator = "";
-                }
+            public void onClick(View v) {
+                int firstNumber = Integer.parseInt(etFirstNumber.getText().toString());
+                int secondNumber =
+                        Integer.parseInt(etSecondNumber.getText().toString());
+                int result = firstNumber + secondNumber;
+                tvResult.setText(result+" ");
             }
         });
-    }
-    // Set click listener for clear button
-    private void setClearButtonClickListener() {
-        Button clearButton = findViewById(R.id.buttonClear);
-        clearButton.setOnClickListener(new View.OnClickListener() {
+        btnSubtract.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                clearCalculator();
+            public void onClick(View v) {
+                int firstNumber = Integer.parseInt(etFirstNumber.getText().toString());
+                int secondNumber =
+                        Integer.parseInt(etSecondNumber.getText().toString());
+                int result = firstNumber - secondNumber;
+                tvResult.setText(result+" ");
             }
         });
-    }
-    // Update result display
-    private void updateResult() {
-        textViewResult.setText(input);
-    }
-    // Calculate result based on operator
-    @SuppressLint("DefaultLocale")
-    private void calculateResult() {
-        double result = 0;
-        switch (operator) {
-            case "+":
-                result = num1 + num2;
-                break;
-            case "-":
-                result = num1 - num2;
-                break;
-            case "*":
-                result = num1 * num2;
-                break;
-            case "/":
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    result = Double.NaN; // Handle division by zero
-                }
-                break;
-        }
-        input = String.format("%.2f", result);
-        updateResult();
-    }
-    // Clear calculator
-    private void clearCalculator() {
-        input = "";
-        num1 = 0;
-        num2 = 0;
-        operator = "";
-        updateResult();
+        btnMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int firstNumber = Integer.parseInt(etFirstNumber.getText().toString());
+                int secondNumber =
+                        Integer.parseInt(etSecondNumber.getText().toString());
+                int result = firstNumber * secondNumber;
+                tvResult.setText(result+" ");
+            }
+        });
+        btnDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int firstNumber = Integer.parseInt(etFirstNumber.getText().toString());
+                int secondNumber =
+                        Integer.parseInt(etSecondNumber.getText().toString());
+                int result = firstNumber / secondNumber;
+                tvResult.setText(result+" ");
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etFirstNumber.setText("");
+                etSecondNumber.setText("");
+                tvResult.setText("");
+            }
+        });
     }
 }
